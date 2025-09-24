9c49ef24cbad2d6e34c06c41d469d51adbc6bcda
Mohammed T. Elgammal <mohammedelgammalamazon@gmail.com>
2025-09-24 18:37:40 +0400

    initial commit

--- packages/shared/const.ts
+++ packages/shared/const.ts
@@ -1,3 +1,3 @@
 export const PROJECT_URL_OBJECT = {
-  url: 'https://github.com/Jonghakseo/chrome-extension-boilerplate-react-vite',
+  url: 'https://github.com/tokenborg/token13-chrome-extension',
 } as const;
